5ff412ef989ea51714246d86c4cb3f004ec14884
juliandeveloper05 <juliansoto.dev@email.com>
2024-01-18 17:51:42 -0300

    Eliminar carpeta chatbot

--- src/components/chatbot/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  botName: "ContactBot",
-  initialMessages: ["¡Hola! ¿Cómo puedo ayudarte hoy?"],
-  // Agrega más configuraciones según sea necesario
-};
-
-export default config;

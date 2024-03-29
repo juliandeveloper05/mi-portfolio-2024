2c4cd5ac6283d064eccbde2606770f257acf922b
juliandeveloper05 <juliansoto.dev@email.com>
2024-03-28 21:41:32 -0300

    i18n solved

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
-    // Estas son todas las configuraciones de locales que deseas admitir en tu aplicación
-    locales: ["en", "es"], // Añade aquí los locales que necesites
-    // Este es el locale predeterminado que deseas que se use cuando se visite una ruta sin prefijo de locale, como `/hello`
+    locales: ["en", "es"],
     defaultLocale: "en",
-    localeDetection: false, // Desactivar la detección automática del idioma
   },
 };

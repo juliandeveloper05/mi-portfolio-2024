767061189d474050305f0ba9a764442fbf483998
juliandeveloper05 <juliansoto.dev@email.com>
2024-04-06 20:28:03 -0300

    Cursor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
 import Head from "next/head";
+import Cursor from "../components/cursor";
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,7 +11,7 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="icon" href="/newlogo.png" />
       </Head>
-
+      <Cursor color="#fff" />
       <Component {...pageProps} />
     </>
   );

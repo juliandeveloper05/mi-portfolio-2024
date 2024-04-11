f3a91d8eb4d555ed98b14fa7404f2b45df185fb7
juliandeveloper05 <juliansoto.dev@email.com>
2024-04-10 21:17:33 -0300

    Latest Updates

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -2,40 +2,46 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { FiSend } from "react-icons/fi";
 import SvgCurve from "./svg-curve";
+import { useTranslation } from "next-i18next";
+import MagneticWrapper from "./magnetic-wrapper";
 
 export default function ContactMe() {
+  const { t } = useTranslation("contact");
+
   return (
-    <section id="contact" className="mb-20 mx-auto sm:mb-28">
+    <section id="contact" className="mb-20 mx-auto sm:mb-28 unselectable">
       <SectionHeading>
-        Contact Me
+        {t("contact1")}
         <SvgCurve />
       </SectionHeading>
       <p className="text-white text-center text-lg leading-relaxed mb-8">
-        Please contact me directly at{" "}
+        {t("contact2")}{" "}
         <a
           className="text-indigo-600 hover:text-indigo-800 transition-colors"
           href="mailto:juliansoto.dev@gmail.com"
         >
           juliansoto.dev@gmail.com
         </a>{" "}
-        or through this form
+        {t("contact3")}
       </p>
       <form className="mt-10 flex flex-col max-w-md mx-auto">
         <input
           type="email"
-          placeholder="Your email address"
-          className="h-14 rounded-lg border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 transition-all px-4 text-gray-700"
+          placeholder={t("contact4")}
+          className="h-14 rounded-lg border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 transition-all px-4 text-black"
         />
         <textarea
-          placeholder="Your message"
-          className="h-52 my-3 rounded-lg border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 transition-all p-4 text-gray-700"
+          placeholder={t("contact5")}
+          className="h-52 my-3 rounded-lg border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 transition-all p-4 text-black"
         />
-        <button
-          type="submit"
-          className="flex items-center justify-center gap-2 h-12 bg-indigo-600 hover:bg-indigo-700 transition-colors text-white rounded-full outline-none px-6 mx-auto sm:mb-10 mb-10"
-        >
-          Submit <FiSend className="text-lg" />
-        </button>
+        <MagneticWrapper>
+          <button
+            type="submit"
+            className="flex items-center justify-center gap-2 h-12 bg-indigo-600 hover:bg-indigo-700 transition-colors text-white rounded-full outline-none px-6 mx-auto sm:mb-10 mb-10"
+          >
+            {t("contact6")} <FiSend className="text-lg" />
+          </button>
+        </MagneticWrapper>
       </form>
     </section>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,6 +28,7 @@ export async function getStaticProps({ locale }: { locale: any }) {
         "profile",
         "about",
         "services",
+        "contact",
       ])),
     },
   };

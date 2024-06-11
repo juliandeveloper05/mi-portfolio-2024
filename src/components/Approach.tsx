16abf915f1c1ba5e6097671b7fc2364785c1845b
juliandeveloper05 <juliansoto.dev@email.com>
2024-06-10 21:23:33 -0300

    updated

--- src/components/Approach.tsx
+++ src/components/Approach.tsx
@@ -24,7 +24,7 @@ const Approach = () => {
             {t("approach1")}
           </h2>
         </h1>
-        <div className="py-6 flex flex-col lg:flex-row items-center justify-center bg-black dark:bg-black w-full gap-4 mx-auto px-8">
+        <div className="py-6 flex flex-col lg:flex-row items-center justify-center   w-full gap-4 mx-auto px-8">
           <Card
             title={t("showme_title1")}
             icon={<AceternityIcon order="showme1" />}
@@ -98,7 +98,7 @@ const Card = ({
       </AnimatePresence>
 
       <div className="relative z-5 unselectable">
-        <div className=" border-white text-center group-hover/canvas-card:-translate-y-4 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] group-hover/canvas-card:opacity-0 transition duration-200">
+        <div className=" text-center group-hover/canvas-card:-translate-y-4 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] group-hover/canvas-card:opacity-0 transition duration-200">
           {icon}
         </div>
         <h2 className="dark:text-white opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4 font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200 text-center text-3xl">
